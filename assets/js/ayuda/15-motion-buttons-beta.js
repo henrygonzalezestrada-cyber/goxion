5d6758e85762dc77e51df1a97ec75a8cd798639a
henrygonzalezestrada-cyber <henrygonzalezestrada@gmail.com>
2026-09-23 00:26:10 -0600

    cleanup: eliminar activador beta Motion Buttons

--- assets/js/ayuda/15-motion-buttons-beta.js
+++
@@ -1,7 +0,0 @@
-(() => {
-    const params = new URLSearchParams(window.location.search);
-    if (params.get("gxMotion") !== "buttons") return;
-
-    document.body.classList.add("gx-motion-buttons-v1");
-    document.documentElement.dataset.gxMotionButtons = "1";
-})();
